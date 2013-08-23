723c3ac2913ab29d6de17f103e1f8cd2daf514cf
John Dickerson <TheSpook@gmail.com>
2013-08-23 03:17:17 -0400

    Cleanup

---
+++ src/edu/cmu/cs/dickerson/kpd/structure/KPDEdge.java
@@ -0,0 +1,9 @@
+package edu.cmu.cs.dickerson.kpd.structure;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+public class KPDEdge extends DefaultWeightedEdge {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/edu/cmu/cs/dickerson/kpd/structure/KPDVertex.java
@@ -0,0 +1,16 @@
+package edu.cmu.cs.dickerson.kpd.structure;
+
+public class KPDVertex {
+
+	private boolean compatible;
+	private boolean altruist;
+	
+	public boolean isCompatible() {
+		return compatible;
+	}
+
+	public boolean isAltruist() {
+		return altruist;
+	}
+	
+}
