6a2230817dc4d2cea7ec45946dffaacd295d003c
John Dickerson <TheSpook@gmail.com>
2014-01-29 18:46:07 -0500

    Skeleton Edge Formulation

--- src/edu/cmu/cs/dickerson/kpd/solver/EdgeFormulationCPLEXSolver.java
+++ src/edu/cmu/cs/dickerson/kpd/solver/EdgeFormulationCPLEXSolver.java
@@ -1,5 +1,9 @@
 package edu.cmu.cs.dickerson.kpd.solver;
 
+import ilog.concert.IloException;
+import edu.cmu.cs.dickerson.kpd.helper.IOUtil;
+import edu.cmu.cs.dickerson.kpd.solver.exception.SolverException;
+import edu.cmu.cs.dickerson.kpd.solver.solution.Solution;
 import edu.cmu.cs.dickerson.kpd.structure.Pool;
 
 public class EdgeFormulationCPLEXSolver extends CPLEXSolver {
@@ -8,4 +12,27 @@ public class EdgeFormulationCPLEXSolver extends CPLEXSolver {
 		super(pool);
 	}
 
+	public Solution solve() throws SolverException {
+
+		IOUtil.dPrintln(getClass().getSimpleName(), "Solving cycle formulation IP.");
+
+		try {
+			super.initializeCPLEX();
+
+			// TODO implement constraint generation
+			
+			cplex.clearModel();
+
+			return null;
+
+		} catch(IloException e) {
+			System.err.println("Exception thrown during CPLEX solve: " + e);
+			throw new SolverException(e.toString());
+		}
+	}
+	
+	@Override
+	public String getID() {
+		return "Edge Formulation CPLEX Solver";
+	}
 }
