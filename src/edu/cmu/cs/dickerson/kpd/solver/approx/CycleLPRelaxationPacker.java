daf0800b73aa35c234b08f2e2a98d075cb6fe207
John Dickerson <TheSpook@gmail.com>
2014-03-24 20:12:53 -0400

    Tweak.

--- src/edu/cmu/cs/dickerson/kpd/solver/approx/CycleLPRelaxationPacker.java
+++ src/edu/cmu/cs/dickerson/kpd/solver/approx/CycleLPRelaxationPacker.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.dickerson.kpd.solver.approx;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -23,26 +24,27 @@ public class CycleLPRelaxationPacker extends Packer {
 	private CycleMembership membership;
 	private Pool pool;
 	private Map<Integer, Double> sortedIndex;
-	
+
 	private boolean isInitialized = false;
-	
+
 	public CycleLPRelaxationPacker(Pool pool, List<Cycle> cycles, CycleMembership membership, boolean doInitialization) {
 		this.pool = pool;
 		this.cycles = cycles;
 		this.membership = membership;
 		if(doInitialization) { init(); }
 	}
-	
+
 	/**
 	 * Gets LP relaxation weights for each cycle in the Pool
 	 */
 	private void init() {
-		
+
 		IOUtil.dPrintln(this.getClass().getSimpleName(), "Doing one-time heavyweight LP relaxation initialization");
-		
+
 		CycleFormulationLPRelaxCPLEXSolver solver = new CycleFormulationLPRelaxCPLEXSolver(this.pool, this.cycles, this.membership);
 		try {
-			this.sortedIndex = MapUtil.sortByValue( solver.solve() );
+			// Sort CycleIdx->Weight in reverse order by value (higher weights first)
+			this.sortedIndex = MapUtil.sortByValue( solver.solve(), true);
 		} catch(SolverException e) {
 			e.printStackTrace();
 			System.exit(-1);
@@ -50,12 +52,12 @@ public class CycleLPRelaxationPacker extends Packer {
 		this.cycles = new ArrayList<Cycle>(cycles);
 		this.isInitialized = true;
 	}
-	
+
 	@Override
 	public Solution pack(double upperBound) {
-		
+
 		if(!isInitialized) { init(); }
-		
+
 		Set<Cycle> matching = new HashSet<Cycle>();
 		double objVal = 0.0;
 
@@ -65,20 +67,31 @@ public class CycleLPRelaxationPacker extends Packer {
 		// Keep track of which vertices are in the matching so far
 		Set<Vertex> matchedVerts = new HashSet<Vertex>();
 
-		// "Sample" randomly from the set of all cycles until either we run out of
-		// cycles, or we hit the parameter upperBound
-		//Collections.shuffle(cycles);
-		for(Integer cycleIdx : sortedIndex.keySet()) {
+		// Iterate from highest to lowest weight cycle in the set,
+		// shuffling little chunks at a time to add some randomization
+		List<Cycle> cycleSubList = new ArrayList<Cycle>();
+		int cycleSubListMaxSize = 10;
 
-			Cycle cycle = cycles.get(cycleIdx);
-			
-			Set<Vertex> cVerts = Cycle.getConstituentVertices(cycle, pool);
+		for(Iterator<Integer> fullCycleIt = sortedIndex.keySet().iterator(); fullCycleIt.hasNext(); ) {
 
-			// If no vertices in this cycle are matched, it's legal to add; add it
-			if(Collections.disjoint(cVerts, matchedVerts)) {
-				matchedVerts.addAll(cVerts);
-				objVal += cycle.getWeight();
-				matching.add(cycle);
+			// Index into full cycle list, add this next cycle to list
+			cycleSubList.add( cycles.get(fullCycleIt.next()) );
+
+			// Every K cycles (or, if we're at the end, fewer than K cycles), pack
+			if(cycleSubList.size() >= cycleSubListMaxSize || !fullCycleIt.hasNext()) {
+
+				Collections.shuffle(cycleSubList);
+				for(Cycle cycle : cycleSubList) {
+					Set<Vertex> cVerts = Cycle.getConstituentVertices(cycle, pool);
+
+					// If no vertices in this cycle are matched, it's legal to add; add it
+					if(Collections.disjoint(cVerts, matchedVerts)) {
+						matchedVerts.addAll(cVerts);
+						objVal += cycle.getWeight();
+						matching.add(cycle);
+					}
+				}
+				cycleSubList.clear();
 			}
 			
 			// If we hit the upper bound, break out
