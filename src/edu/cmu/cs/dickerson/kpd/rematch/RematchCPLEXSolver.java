931bde90192f0b6550337254e660d78b0e069451
John Dickerson <TheSpook@gmail.com>
2015-01-31 21:41:51 -0500

    Adaptive deterministic bugfix
    Constrain out cycles that were in previous matchings.

--- src/edu/cmu/cs/dickerson/kpd/rematch/RematchCPLEXSolver.java
+++ src/edu/cmu/cs/dickerson/kpd/rematch/RematchCPLEXSolver.java
@@ -199,7 +199,12 @@ public class RematchCPLEXSolver extends CPLEXSolver {
 							cycleIdx++;
 						}
 					}
-					
+					// Also constrain out any cycles we had in the last match
+					for(Integer cycleColID : lastMatchCycleIdxSet) {
+						IloLinearNumExpr sum = cplex.linearNumExpr(); 
+						sum.addTerm(1.0, x[cycleColID]);
+						cplex.addEq(sum, 0.0);
+					}
 					break;
 				default:
 					throw new SolverException("Have not implemented RematchType " + rematchType + " yet");
